7d50515b7c72a2ba357c97976798fc4bb327cff4
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-03-04 19:48:19 +0100

    mock mock..

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -4,6 +4,7 @@ __metaclass__ = type
 
 import unittest
 import mock
+from . import MOCK_WINDOW1
 from . import MOCK_WINDOWS_LIST
 from . import MOCK_WORKSPACES_LIST
 from . import MOCK_OUTPUTS_LIST
@@ -75,6 +76,8 @@ class TestMenu(unittest.TestCase):
         menu = OutputsMenu('test')
         menu.i3.get_active_outputs = mock.MagicMock(
             return_value=MOCK_OUTPUTS_LIST)
+        menu.i3.get_focused_window = mock.MagicMock(
+            return_value=MOCK_WINDOW1)
         # this is due to the addiction of the 4 xrandr directions
         self.assertEqual(len(menu.entries), len(MOCK_OUTPUTS_LIST) + 4)
         self.assertEqual(menu.entries[-1].value.name, 'MockOutput2')
