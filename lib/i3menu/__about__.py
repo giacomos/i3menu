be071b85556c036709e8c46bfea8a06355a09fb2
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-03-07 11:54:19 +0100

    better version output

--- lib/i3menu/__about__.py
+++ lib/i3menu/__about__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 # https://github.com/pypa/packaging/blob/master/packaging/__about__.py
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
-    "__email__", "__license__", "__copyright__",
+    "__email__", "__license__", "__longlicense__" "__copyright__",
 ]
 
 __title__ = "i3menu"
@@ -16,4 +16,6 @@ __author__ = "Giacomo Spettoli"
 __email__ = "giacomo.spettoli@gmail.com"
 
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2016 %s" % __author__
+__longlicense__ = "License GPLv3+: GNU GPL version 3" \
+    " or later <http://gnu.org/licenses/gpl.html>."
+__copyright__ = "Copyright (C) 2016 %s" % __author__
--- lib/i3menu/__init__.py
+++ lib/i3menu/__init__.py
@@ -6,13 +6,13 @@ from i3menu import i18n
 import logging
 
 from .__about__ import (
-    __author__, __copyright__, __email__, __license__, __summary__, __title__,
-    __uri__, __version__
+    __author__, __copyright__, __email__, __license__, __longlicense__,
+    __summary__, __title__, __uri__, __version__
 )
 
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
-    "__email__", "__license__", "__copyright__",
+    "__email__", "__license__", "__longlicense__", "__copyright__",
 ]
 
 logger = logging.getLogger()
